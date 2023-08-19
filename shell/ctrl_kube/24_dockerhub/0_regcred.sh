fa1fe3f38bec0b9e8cc0d7c1746c5c9535ce1987
kkumtree <bc0569@naver.com>
2023-08-19 12:41:42 +0900

    add: kubeadm ctrl success

--- shell/ctrl_kube/24_dockerhub/0_regcred.sh
+++ shell/ctrl_kube/24_dockerhub/0_regcred.sh
@@ -15,12 +15,15 @@ fi
 source $DOT_ENV
 
 # Check if variables are set
-if [[ -z "$DOCKER_SERVER_PODMAN" || -z "$DOCKER_USERNAME" || -z "$DOCKER_PASSWORD" ]]; then
+if [[ -z "$DOCKER_SERVER" || -z "$DOCKER_USERNAME" || -z "$DOCKER_PASSWORD" ]]; then
   echo "ERROR: DOCKER_{ SERVER_PODMAN || USERNAME || PASSWORD } not set in the .env file."
   exit 1
 fi
 
+kubectl create namespace kube-cred
+
 kubectl create secret docker-registry regcred \
-  --docker-server=$DOCKER_SERVER_PODMAN \
+  --namespace=kube-cred \
+  --docker-server=$DOCKER_SERVER \
   --docker-username=$DOCKER_USERNAME \
   --docker-password=$DOCKER_PASSWORD
